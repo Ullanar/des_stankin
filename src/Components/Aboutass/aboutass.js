61709d10f834031e310b48307c343e94065f16e9
Alex Осипов <lexer2002@mail.ru>
2020-12-16 15:51:40 +0300

    add description card

--- src/Components/Aboutass/aboutass.js
+++ src/Components/Aboutass/aboutass.js
@@ -8,35 +8,49 @@ function AboutAss() {
     const imgSources = [
         {
             src: 'images/lev.jpg',
-            text: 'TeamLead'
+            name: 'Лев Бубнов',
+            text:'-Full Stack Dev.',
+            text1:'-Team Lead',
+            description:'Telegram: @ullanar'
         },
         {
             src: 'images/Лиза А.jpg',
-            text: 'лвлылтьытыоыовоу',
+            name: 'Акманова Лиза',
+            text: '-FrontEnd Dev.',
+            text1: '-QA',
+            description:'Telegram: @akmanova'
         },
         {
             src: 'images/Лиза Ш.jpg',
-            text: 'Text',
+            name: 'Шайгородская Лиза',
+            text: '-FrontEnd Dev.',
+            text1: '-UI/UX',
+            description:'Telegram: @shveyga'
         },
         {
             src: 'images/Леха.jpg',
-            text: 'Text',
+            name: 'Осипов Алексей',
+            text: '-DevOps',
+            text1: '-BackEnd Dev.',
+            description:'Telegram: @AlexOsi02'
         },
     ]
     return (
         <div style={wrapperStyle}>
-            <h1 align="center"><b>Кто мы такие?</b></h1>
-            <p><h1>Мы перспективная команда, которая изменит мир образования в лучшую сторону!</h1></p>
-            <h2 align="left">TeamLead</h2>
+            <h1 align="center"><b>About Us</b></h1>
+            <p><h2 align="center">Мы перспективная команда разработчиков, которая изменит мир дистанционного образования в лучшую сторону!</h2></p>
             <Row justify='center' wrap>
                 {imgSources.map((item, i) => (
                     <Col className="gutter-row" span={5}>
                         <Card
                             hoverable
-                            style={{ width: 200 }}
+                            style={{ width: 210 }}
                             cover={<img alt="" src={item.src}/>}
                         >
+                            <Meta title={item.name}/>
                             <Meta title={item.text}/>
+                            <Meta title={item.text1}/>
+                            <Meta description={item.description}/>
                         </Card>
                     </Col>
                 ))}
