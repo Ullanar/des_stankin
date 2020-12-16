54ed3b72f175b922d3b550344690ab324ca4a118
Ullanar <lev.boubnov@gmail.com>
2020-12-16 11:51:49 +0300

    add titles in messages sidebar

--- src/Components/Messages/PersonalMessages/MessagesSidebar/messagesSidebar.js
+++ src/Components/Messages/PersonalMessages/MessagesSidebar/messagesSidebar.js
@@ -1,15 +1,102 @@
 import React from "react";
 import css from './messagesSidebar.module.css'
 import {Input} from 'antd';
+import {Avatar} from 'antd';
+
 const {Search} = Input;
 
 
 function MessagesSidebar(props) {
     return (
-        <div>            <Search placeholder="Введите имя собеседника" onSearch={value => console.log(value)} enterButton/>
+        <div>
+            <Search placeholder="Введите имя собеседника" onSearch={value => console.log(value)} enterButton/>
 
             <div className={css.scrollbox}>
-                {/*{props.rooms}*/}
+
+                <div className={css.Titles}>
+
+                        <Avatar size={40}>USER</Avatar>
+                        <div className={css.Titles_info}>
+                            <h2>Комната 1</h2>
+                            <p>Last message</p>
+                        </div>
+
+                </div>
+
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+                <div className={css.Titles}>
+
+                    <Avatar size={40}>USER</Avatar>
+                    <div className={css.Titles_info}>
+                        <h2>Комната 1</h2>
+                        <p>Last message</p>
+                    </div>
+
+                </div>
+
 
             </div>
         </div>
